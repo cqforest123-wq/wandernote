8c835fba70ae76092f2762de35540ba7305ac610
Litao <15278956@qq.com>
2026-09-17 15:11:50 +0800

    chore: refresh watch data on active notification

--- ios/TravelWatchCompanion Watch App/TravelWatchCompanionApp.swift
+++ ios/TravelWatchCompanion Watch App/TravelWatchCompanionApp.swift
@@ -1,8 +1,8 @@
 import SwiftUI
+import WatchKit
 
 @main
 struct TravelWatchCompanionApp: App {
-    @Environment(\.scenePhase) private var scenePhase
     @StateObject private var runtime = WatchCompanionRuntime()
 
     var body: some Scene {
@@ -13,10 +13,12 @@ struct TravelWatchCompanionApp: App {
                 .task {
                     runtime.start()
                 }
-                .onChange(of: scenePhase) { newPhase in
-                    if newPhase == .active {
-                        runtime.refreshActiveData()
-                    }
+                .onReceive(
+                    NotificationCenter.default.publisher(
+                        for: WKApplication.didBecomeActiveNotification
+                    )
+                ) { _ in
+                    runtime.refreshActiveData()
                 }
         }
     }
